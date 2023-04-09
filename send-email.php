927dec61fb72e22994fda2a38e17bf50bc6fac0d
victorialange <viclan2009@hotmail.com>
2023-04-09 10:06:49 -0400

    Store data from new account & lost password forms and render message conditionally

--- send-email.php
+++ send-email.php
@@ -13,7 +13,34 @@
   <?php
     // if emailType = newAccount-> just display new account msg (Your Account has been created successfully)
     // if emailType = lostPassword-> just display lost password msg (Your Password has been reset/retrieved/your new password is on its way)
+    // checking if post variable is set
+    if (isset($_POST['submit'])) {
+      // store post variable for emailType retrieved from lost password and/or new account form upon submission
+      $emailType = $_POST['emailType'];
+    }
   ?>
+
+  <main>
+    <section class="sendEmailMain">
+      <div class="wrapper">
+        <!-- ->Button to Logout.php across all pages -->
+        <!-- log out - redirect to log out page - destroy session -->
+        <a href="logout.php" class="logoutLink">Log out</a>
+        <h2>Send email</h2>
+        <hr>
+
+        <!-- if emailType = newAccount-> just display new account msg  -->
+        <?php if ( $emailType == "newAccount" ) { ?> 
+          <h3>New Account was created successfully.</h3>    
+        <?php } ?>
+
+        <!-- if emailType = lostPassword-> just display lost password msg -->
+        <?php if ( $emailType == "lostPassword" ) { ?> 
+          <h3>Your new password is being sent to your email.</h3>    
+        <?php } ?>
+      </div>
+    </section>
+  </main>
 
   <!-- include footer.php for copyright -->
   <?php include 'footer.php'; ?>
