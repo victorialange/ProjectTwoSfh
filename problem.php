128bf12e6dd30f42af5f6cb3c1e2586f4d80855b
victorialange <viclan2009@hotmail.com>
2023-04-09 10:01:31 -0400

    Replace original variables with session variables & add section

--- problem.php
+++ problem.php
@@ -10,8 +10,8 @@
   <!-- include header.php for header & side nav -->
   <?php include 'header.php'; ?>
 
+  <!-- storing form data -->
   <?php
-    echo "Links";
     // if role= 'admin' -> link to: new-account.php and isnt-working.php 
     // if role = 'manager' ->link to: lost-password.php and isnt-working.php 
     // if role= 'ceo' ->link to: need-help.php and isnt-working.php 
@@ -26,54 +26,62 @@
     }
   ?>
 
-  <!-- greeting according to rule -->
-  <h2>Hello <?php echo $role; ?></h2>
+  <main>
+    <section class="problemMain">
+      <div class="wrapper">
+        <!-- ->Button to Logout.php across all pages -->
+        <!-- log out - redirect to log out page - destroy session -->
+        <a href="logout.php" class="logoutLink">Log out</a>
 
-  <h3>Here are your options:</h3>
+        <!-- greeting according to rule -->
+        <h2>Hello <?php echo $_SESSION['role']; ?></h2>
+        <hr>
+        <h3>Here are your options:</h3>
 
-  <!-- if role= 'admin' -> link to: new-account.php and isnt-working.php  -->
-  <?php if ( $role == "Admin" ) { ?> 
-    <ul>
-      <!-- redirect to computer isn't working -->
-      <li>
-        <a href="isnt-working.php">My computer isn't working</a>
-      </li>
-      <!-- redirect to create new account -->
-      <li>
-        <a href="new-account.php">Create New Account</a>
-      </li>
-    </ul>
-  <?php } ?>
+        <!-- if role= 'admin' -> link to: new-account.php and isnt-working.php  -->
+        <?php if ( $_SESSION['role'] == "Admin" ) { ?> 
+          <ul>
+            <!-- redirect to computer isn't working -->
+            <li>
+              <a href="isnt-working.php">My computer isn't working</a>
+            </li>
+            <!-- redirect to create new account -->
+            <li>
+              <a href="new-account.php">Create New Account</a>
+            </li>
+          </ul>
+        <?php } ?>
 
-  <!-- if role = 'manager' ->link to: lost-password.php and isnt-working.php  -->
-  <?php if ( $role == "Manager" ) { ?> 
-    <ul>
-      <!-- redirect to computer isn't working -->
-      <li>
-        <a href="isnt-working.php">My computer isn't working</a>
-      </li>
-      <!-- redirect to lost-password -->
-      <li>
-        <a href="lost-password.php">Lost Password</a>
-      </li>
-    </ul>
-  <?php } ?>
+        <!-- if role = 'manager' ->link to: lost-password.php and isnt-working.php  -->
+        <?php if ( $_SESSION['role'] == "Manager" ) { ?> 
+          <ul>
+            <!-- redirect to computer isn't working -->
+            <li>
+              <a href="isnt-working.php">My computer isn't working</a>
+            </li>
+            <!-- redirect to lost-password -->
+            <li>
+              <a href="lost-password.php">Lost Password</a>
+            </li>
+          </ul>
+        <?php } ?>
 
-  <!-- if role= 'ceo' ->link to: need-help.php and isnt-working.php -->
-  <?php if ( $role == "CEO" ) { ?> 
-    <ul>
-       <!-- redirect to computer isn't working -->
-      <li>
-        <a href="isnt-working.php">My computer isn't working</a>
-      </li>
-      <!-- redirect to need help -->
-      <li>
-        <a href="need-help.php">Need Help</a>
-      </li>
-    </ul>
-  <?php } ?>
-
-
+        <!-- if role= 'ceo' ->link to: need-help.php and isnt-working.php -->
+        <?php if ( $_SESSION['role'] == "CEO" ) { ?> 
+          <ul>
+            <!-- redirect to computer isn't working -->
+            <li>
+              <a href="isnt-working.php">My computer isn't working</a>
+            </li>
+            <!-- redirect to need help -->
+            <li>
+              <a href="need-help.php">Need Help</a>
+            </li>
+          </ul>
+        <?php } ?>
+      </div>
+    </section>
+  </main>
   <!-- include footer.php for copyright -->
   <?php include 'footer.php'; ?>
 </body>
