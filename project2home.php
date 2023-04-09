6564c2cfbdf30d6c36378e79c9ac54eb9d1f8967
victorialange <viclan2009@hotmail.com>
2023-04-09 06:45:09 -0400

    Reformat code

--- project2home.php
+++ project2home.php
@@ -20,41 +20,41 @@
       <!-- form heading -->
       <h2>Welcome To Your IT Support System</h2>
 
-  <!-- initial form with 4 fields -->
-  <!-- send values of form to problem.php -->
+      <!-- initial form with 4 fields -->
+      <!-- send values of form to problem.php -->
       <form action="problem.php" method="post" class="supportForm">
-    <!-- 1st field: dropdown menu for title -->
+        <!-- 1st field: dropdown menu for title -->
         <!-- label only visible to screen readers for accessibility -->
         <label for="title" class="sr-only">Title</label>
-    <select name="title" id="title" required>
-      <option value="mr">Mr</option>
-      <option value="ms">Ms</option>
-      <option value="mrs">Mrs</option>
-    </select>
+        <select name="title" id="title" required>
+          <option value="mr">Mr</option>
+          <option value="ms">Ms</option>
+          <option value="mrs">Mrs</option>
+        </select>
 
-    <!-- 2nd field: input bar for first name -->
+        <!-- 2nd field: input bar for first name -->
         <!-- label only visible to screen readers for accessibility -->
         <label for="fName" class="sr-only">First Name</label>
         <input type="text" name="fName" id="fName" placeholder="First Name" required>
 
-    <!-- 3rd field: input bar for last name -->
+        <!-- 3rd field: input bar for last name -->
         <!-- label only visible to screen readers for accessibility -->
         <label for="lName" class="sr-only">Last Name</label>
         <input type="text" name="lName" id="lName" placeholder="Last Name" required>
 
-    <!-- 4th field: dropdwon menu for role -->
+        <!-- 4th field: dropdwon menu for role -->
         <!-- label only visible to screen readers for accessibility -->
         <label for="role" class="sr-only">Role</label>
-    <select name="role" id="role" required>
-      <option value="Admin">Admin</option>
-      <option value="Manager">Manager</option>
-      <option value="CEO">CEO</option>
-    </select>
+        <select name="role" id="role" required>
+          <option value="Admin">Admin</option>
+          <option value="Manager">Manager</option>
+          <option value="CEO">CEO</option>
+        </select>
 
-    <!-- submit button -->
-    <button type="submit">Submit</button>
+        <!-- submit button -->
+        <button type="submit">Submit</button>
 
-  </form>
+      </form>
     </div>
   </div>
 
