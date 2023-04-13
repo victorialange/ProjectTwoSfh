47709d103d5c448a7eff6c651ab831502e102bf6
victorialange <viclan2009@hotmail.com>
2023-04-13 08:09:33 -0400

    Redirect user to initial IT support form page with JS & add delay in head

--- logout.php
+++ logout.php
@@ -6,6 +6,7 @@
     session_unset();
     session_destroy();
     // this will redirect the user again to the "login page/ the support form"
+    echo "<script> location.href='project2home.php';; </script>"; 
     exit();
   }
 ?>
@@ -16,6 +17,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- have logout page be displayed for 3.5 seconds before redirect -->
+  <meta http-equiv="refresh" content="3.5">
   <title>Halifax Canoe and Kayak</title>
 </head>
 <body>
