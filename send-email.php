b0a8bfd87c6c89c1a8d19bd514a92809153154a2
victorialange <viclan2009@hotmail.com>
2023-04-14 03:33:19 -0400

    Add extra redirect to email confirmation page taking user back to problem.php with 3s delay based on whether session variable is set

--- send-email.php
+++ send-email.php
@@ -1,4 +1,9 @@
-<?php include './includes/header.php'; ?>
+<?php 
+include './includes/header.php';
+if (isset($_SESSION['redirect'])) {
+  header("Location: problem.php"); 
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,6 +11,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- have send email page be displayed for 3 seconds before redirect back to problem.php -->
+  <meta http-equiv="refresh" content="3">
   <link href="style.css" rel="stylesheet" type="text/css" />
   <title>Send Email</title>
   <!-- logo  -->
@@ -40,3 +47,6 @@
   <?php include './includes/footer.php'; ?>
 </body>
 </html>
+<?php 
+  $_SESSION['redirect']="redirect";
+?>
