b4c94ae9065ad49142e3ed184d21873108d6f84c
victorialange <viclan2009@hotmail.com>
2023-04-14 02:02:19 -0400

    Initialize session variables & assign them to post variable values retrieved from first form based on whether form data empty or set, set loggedIn & run session variable to true

--- problem.php
+++ problem.php
@@ -1,4 +1,59 @@
-<?php include './includes/header.php'; ?>
+<?php include './includes/header.php'; 
+  // checking if session run is false (only initialize variables and populate session variables with form data when session isn't run)
+  if (!isset($_SESSION["run"]) || $_SESSION["run"] !== true) {
+    // initialize variables with empty values
+    $title = "";
+    $fName = "";
+    $lName = "";
+    $role = "";
+    $emailType= "";
+
+    // checking if data posted from form is set
+    if (isset($_POST['title']) && isset($_POST['fName']) && isset($_POST['lName']) && isset($_POST['role'])) {
+      // checking that data isn't empty
+      if (!empty($_POST['title']) && !empty($_POST['fName']) && !empty($_POST['lName']) && !empty($_POST['role'])) {
+        // retrieving post variables from form
+        // storing values from post variable in initialized variables from above
+        $title = $_POST["title"];
+        $fName = $_POST['fName'];
+        $lName = $_POST['lName'];
+        $role = $_POST['role'];
+          
+        // if post variable for emailType is set from forms store into initialized emailType variable 
+        if (isset($_POST['emailType'])){
+          $emailType = $_POST['emailType'];
+        }
+      }
+    }
+    
+    // populating session variables with values
+    // user is logged in upon form submission
+    $_SESSION["loggedIn"] = true;
+    // settting session variables equal to values retrieved from post variables from form submission
+    $_SESSION['title'] = $title;
+    $_SESSION['fName'] = $fName;
+    $_SESSION['lName'] = $lName;
+    $_SESSION['role'] = $role;
+    // session is now run upon form submission
+    $_SESSION["run"] = true;
+
+    // setting emailType based on selected role from form
+    if ($_SESSION['role'] == "Admin") {
+      $_SESSION['emailType'] = "newAccount";
+      header("location: problem.php");
+    }
+
+    if ($_SESSION['role'] == "Manager") {
+      $_SESSION['emailType'] = "lostPassword";
+      header("location: problem.php");
+    }
+
+    if ($_SESSION['role'] == "CEO") {
+      $_SESSION['emailType'] = "needHelp";
+      header("location: problem.php");
+    }
+  }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,23 +65,6 @@
   <link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-
-  <!-- storing form data -->
-  <?php
-    // if role= 'admin' -> link to: new-account.php and isnt-working.php 
-    // if role = 'manager' ->link to: lost-password.php and isnt-working.php 
-    // if role= 'ceo' ->link to: need-help.php and isnt-working.php 
-
-    // checking if post variable is set
-    if (isset($_POST['submit'])) {
-      // store session data by setting it equal to its post variables retrieved from the form upon submission
-      $_SESSION['title'] = $_POST['title'];
-      $_SESSION['fName'] = $_POST['fName'];
-      $_SESSION['lName'] = $_POST['lName'];
-      $_SESSION['role'] = $_POST['role'];
-    }
-  ?>
-  
   <?php include './includes/nav-bar.php';?>
   <main>
     <section class="problemMain">
