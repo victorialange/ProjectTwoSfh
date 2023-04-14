ae587f10d84e439bdb13b73c99391d706cad06f9
victorialange <viclan2009@hotmail.com>
2023-04-14 03:09:46 -0400

    Update nav links to point correctly to relevant pages

--- includes/nav-bar.php
+++ includes/nav-bar.php
@@ -20,8 +20,9 @@
 
 <!-- hidden sidenav -->
 <nav class="sideNavHidden">
-    <ul class="sideNavContent">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="#">Book Trip</a></li>
-    </ul>
+  <ul class="sideNavContent">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="#">Book Trip</a></li>
+    <li><a href="project2-home.php">IT Support</a></li>
+  </ul>
 </nav>
